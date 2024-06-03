0abdff07c0edf69c31fd41842400ab1af540ae43
junw <wo1261931780@gmail.com>
2024-06-03 16:12:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,24 @@
-<script setup>
-// 上面的setup，就是一个开关，允许我们内部书写组合式api
-// 组合式api的意思是，一系列的函数
-// 这些函数都是在setup选项中书写的
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-// 相当于是我们在生成的时候,优先执行了setup函数
+<script>
+export default {
+  setup() {
+    console.log('Hello World')
+    const message = 'junw'
+    const logMessage = () => console.log(message)
+    return{
+      message,
+      logMessage,
+      // 方法和对象都需要打印出来才可以使用
+      // 语法糖，经过一定的封装，将复杂写法变成简单写法
+    }
+  },
+  beforeCreate() {
+    console.log('Hello World')
+  }
+}
 </script>
 
 <template>
-<!--  模板内不再要求唯一跟元素-->
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>123</div>
+  <div>{{ message }}</div>
+  <button @click="logMessage()">Log Message</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
