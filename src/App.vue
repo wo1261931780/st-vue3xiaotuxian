f2384bcae2c83ea5079be0d883a07d068e676442
junw <wo1261931780@gmail.com>
2024-06-03 21:48:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const list = ref([1, 2, 3, 4, 5])
+// ref监听的不需要使用.value去处理
+const demo = ref(0)
+const demoCount = () => {
+  demo.value++
+  console.log(demo.value)
+}
 import { computed } from 'vue'
 
 const computedList = computed(() => {
@@ -12,13 +18,47 @@ setTimeout(() => {
   list.value.push(6)
   // 向数组中插入数字6
 }, 3000)
+
+// watch(list, (newVal, oldVal) => {
+//   console.log('list发生了变化', newVal, oldVal)
+// })
+// watch(demo, (newVal, oldVal) => {
+//   console.log('demo', newVal, oldVal)
+// })
+// 如果要同时监听多个数据，使用数组的形式就可以
+// watch([list, demo], ([newVal, newName], [oldVal, oldName]) => {
+//     console.log('list或demo发生了变化', [newVal, newName], [oldVal, oldName])
+//   }
+// )
+// watch([list, demo], ([newVal, newName], [oldVal, oldName]) => {
+//     console.log('list或demo发生了变化', [newVal, newName], [oldVal, oldName])
+//   },
+//   {
+//     immediate: true
+//     // 在创建的时候就执行一次,注意是watch内部
+//   }
+// )
+// 上面我们会发现，list的变化不会被监听到，因为默认是浅层监听的，这里不会进行回调
+// watch([list, demo], ([newVal, newName], [oldVal, oldName]) => {
+//     console.log('list或demo发生了变化', [newVal, newName], [oldVal, oldName])
+//   },
+//   {
+//     deep: true
+//   }
+// )
+// 上面使用递归进行深层监听，如果一个对象中有任意属性变化，都会监听到
 </script>
 
 <template>
   <div>
-    {{ list.value }}
+    我是list.value：{{ list }}
   </div>
+  <br>
   <div>
-    {{ computedList }}
+    我是fancyList：{{ computedList }}
   </div>
+  <br>
+  <button @click = "demoCount">
+    我是demo：{{ demo }}
+  </button>
 </template>
