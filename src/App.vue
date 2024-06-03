3eaf6fbf8b309794ef47e1163a0ee5407416bb30
junw <wo1261931780@gmail.com>
2024-06-03 16:28:35 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
-import { reactive } from 'vue'
-// 这里通过reactive函数将state对象包装成响应式对象，使得其中的属性可以响应式地绑定到视图上。
-// 说人话就是，state对象中的属性值发生变化时，视图也会自动更新。
-// 本来count是data中的一个普通变量，现在变成了响应式变量，可以直接绑定到视图上。
-const state = reactive({
-  count: 0
-})
+import { ref } from 'vue'
+// ref稍有不同，可以支持简单类型和对象类型
+const count = ref(0)
 
-const increase = () => {
-  state.count++
+const increase=()=>{
+  count.value++
+  // 这里就必须通过.value来获取数据，然后进行修改
 }
+
 </script>
 
 <template>
   <div>
-    <button @click="increase">{{ state.count }}</button>
+    <button @click = "increase">{{ count }}</button>
   </div>
 </template>
