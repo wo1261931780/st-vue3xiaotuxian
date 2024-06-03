1d54400665340c04b29f4e1e85ac7de718230565
junw <wo1261931780@gmail.com>
2024-06-03 16:16:57 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
-<script>
-export default {
-  setup() {
-    console.log('Hello World')
-    const message = 'junw'
-    const logMessage = () => console.log(message)
-    return{
-      message,
-      logMessage,
-      // 方法和对象都需要打印出来才可以使用
-      // 语法糖，经过一定的封装，将复杂写法变成简单写法
-    }
-  },
-  beforeCreate() {
-    console.log('Hello World')
-  }
-}
+<script setup>
+const message = 'Hello World!'
+const logMessage = () => console.log(message)
 </script>
 
 <template>
